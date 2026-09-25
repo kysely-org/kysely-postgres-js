5abcc1aef5ee670a692fba895eee9262c4bf6481
Igal Klebanov <igalklebanov@gmail.com>
2026-09-25 10:40:39 +0300

    ...

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
-	attw: {
-		enabled: true,
-		profile: 'esm-only',
-	},
+	attw: true,
 	entry: 'src/index.ts',
 	exports: true,
 	publint: true,
